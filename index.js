2d1f6ba89b4f31225797455e3cd4d2e62f4af6e3
MD-SelimReza <rezaselim405@gmail.com>
2024-05-12 17:43:10 +0600

    Successfully get all post posted by a specific user

--- index.js
+++ index.js
@@ -66,6 +66,14 @@ async function run() {
             res.send(result);
         })
 
+        // Get all post posted by a specific user
+        app.get('/posts/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { organizer_email: email };
+            const result = await postCollection.find(query).toArray();
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
